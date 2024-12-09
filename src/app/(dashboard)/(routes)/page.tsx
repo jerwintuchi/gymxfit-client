535883b274f637d6b550f72cf1872a933418d1ba
Jerwin Peralta <jerwin.tuchi3@gmail.com>
2024-12-09 15:57:32 +0800

    added UserButton from Clerk

--- src/app/(dashboard)/(routes)/page.tsx
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 export default function page() {
     return (
-        <div>page</div>
+        <div>
+            <UserButton />
+        </div>
     )
 }
