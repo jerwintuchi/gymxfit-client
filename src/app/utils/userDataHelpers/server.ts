571fcda3aa5d13a829d08f165698bba5ff731b74
Jerwin Peralta <jerwin.tuchi3@gmail.com>
2025-01-05 18:29:51 +0800

    added global CustomJwtSessionClaims in types folder and moved the Roles type

--- src/app/utils/userDataHelpers/server.ts
+++ src/app/utils/userDataHelpers/server.ts
@@ -1,7 +1,6 @@
 "use server"
 
-import { UserContextType } from "@/app/contextProviders/UserProvider";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 
 export const getUserId = async () => {
     const { userId } = await auth();
@@ -11,19 +10,4 @@ export const getUserId = async () => {
     return userId;
 };
 
-export const getUserFromServer = async (): Promise<UserContextType> => {
-    const user = await currentUser()
-    if(!user) {
-        throw new Error("Cannot get user")
-    }
-    console.log("user from getUserFromServer()", user) 
-    return {
-        userId: user.id,
-        role: user.publicMetadata.role,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.emailAddresses[0].emailAddress,
-        profilePicture: user.imageUrl
-    } as UserContextType
-}/* cherry picked type from the User interface from clerk since I only want these fields for the UserContext*/
 
